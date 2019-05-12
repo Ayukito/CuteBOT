517e33e69240af63557f00c74181bec59d8ffacc
Allison <alexbrowses@gmail.com>
2019-05-12 01:31:47 -0400

    restart -> stop

--- commands/Administration/stop.js
+++ commands/Administration/stop.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-	name: "restart",
-	description: "Restart bot (bot owners only)",
+	name: "stop",
+	aliases: ["restart"],
+	description: "Stop bot (bot owners only, restarts sometimes lol)",
 	execute(message, args) {
 		console.log(message.author.id);
 		if (message.author.id == 312309974384640011) {
-			message.channel.send("Shutting down bot. (restarting if using pm2)")
+			message.channel.send("Shutting down bot. (restarts sometimes lol)")
 				.then(()=>{
 					process.exit();
 				});
