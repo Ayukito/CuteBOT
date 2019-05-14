a42c0821b6e79dcb739fa5068ae9b567f3d2810a
Allison <alexbrowses@gmail.com>
2019-05-14 11:08:36 -0400

    Update MESSAGE_CREATE.js

--- events/MESSAGE_CREATE.js
+++ events/MESSAGE_CREATE.js
@@ -1,11 +1,13 @@
 module.exports = {
 	execute(client, Discord, packet) {
 		console.log("message");
-		console.log(packet.toString());
-		client.channels.fetch(packet.d.channel_id).then(channel =>{
-			console.log("CHANNEL");
-			channel.messages.fetch(packet.d.message_id)
-				.then(message => console.log(message.content))
+		console.log(packet.d);
+		console.log(packet.d.message_id);
+		const channel_id = packet.d.channel_id;
+		const message_id = packet.d.message_id;
+		client.channels.fetch(channel_id).then(channel =>{
+			channel.messages.fetch(message_id)
+				.then(console.log)
 				.catch(console.error);
 		});
 	}
