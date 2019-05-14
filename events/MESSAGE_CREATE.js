29e1acd09d5e792c9a9127888a50c7f5b8d98c0b
Allison <alexbrowses@gmail.com>
2019-05-14 10:56:42 -0400

    Update MESSAGE_CREATE.js

--- events/MESSAGE_CREATE.js
+++ events/MESSAGE_CREATE.js
@@ -3,10 +3,9 @@ module.exports = {
 		console.log("message");
 		console.log(packet.toString());
 		const channel = client.channels.get(packet.d.channel_id);
-		const message = channel.fetch(packet.d.message_id);
-		console.log(message);
-		for (var i in message) {
-			console.log(i + ": " + message[i]);
-		}
+		channel.fetch(packet.d.message_id).then(message =>{
+			console.log(message);
+			console.log(message.author);
+		});
 	}
 };
