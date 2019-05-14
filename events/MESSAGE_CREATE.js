17abd66cb4815191db1ced0c941713a9cbe43e28
Allison <alexbrowses@gmail.com>
2019-05-14 10:50:34 -0400

    s

--- events/MESSAGE_CREATE.js
+++ events/MESSAGE_CREATE.js
@@ -1,6 +1,9 @@
 module.exports = {
 	execute(client, Discord, packet) {
+		console.log("message");
 		console.log(packet.toString());
-		
+		const channel = client.channels.get(packet.d.channel_id);
+		const message = channel.fetch(packet.d.message_id);
+		console.log(channel,message);
 	}
 };
